c66f43e340c0a4aa599229002aeb344adde80f16
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-03-06 14:08:26 +0300

    Add print_buffer function prototype

--- alx-low_level_programming/0x06-pointers_arrays_strings/main.h
+++ alx-low_level_programming/0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -17,5 +18,6 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
