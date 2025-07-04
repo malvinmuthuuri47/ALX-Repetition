64ecd54669a2ef30bc8583686a81eae9ebebe38b
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2025-07-04 09:44:38 +0300

    Add preprocessor directive solutions

---
+++ alx-low_level_programming/0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef _ABS_MACRO_H_
+#define _ABS_MACRO_H_
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
---
+++ alx-low_level_programming/0x0D-preprocessor/4-sum.h
@@ -0,0 +1,7 @@
+#ifndef _SUM_FUNCTION_MACRO_
+#define _SUM_FUNCTION_MACRO_
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
+
